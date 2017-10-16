4ae9351a3176d1b69c36221a0a5694325fe2fc34
Shaun Karran <shaun.karran@biarri.com>
2017-10-17 08:19:28 +1000

    Beginning threadpool implementation.

---
+++ src/bin/server.rs
@@ -0,0 +1,17 @@
+extern crate threadpool;
+
+use std::net::TcpListener;
+use threadpool::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("localhost:8080").unwrap();
+    let pool = ThreadPool::new(10);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(move || {
+            println!("{:?}", stream);
+        });
+    }
+}
