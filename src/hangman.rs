0ba0042a6e24edd8e6ce012e6e49c8af4f4b122e
Shaun Karran <shaun.karran@biarri.com>
2017-10-27 22:46:54 +1000

    Add sets to hangman game to easily determine if all characters have been guessed.

--- src/hangman.rs
+++ src/hangman.rs
@@ -1,21 +1,27 @@
 use std::cmp::min;
 use std::collections::HashSet;
+use std::iter::FromIterator;
 
 pub struct Game {
     word: String,
-    guesses: Vec<u8>,
+    word_character_set: HashSet<char>,
     number_allowed_guesses: usize,
+    guesses: Vec<char>,
+    guesses_character_set: HashSet<char>,
 }
 
 impl Game {
     pub fn new(word_set: HashSet<String>) -> Game {
         let word = Game::select_word(word_set);
+        let word_character_set = HashSet::from_iter(word.chars());
         let number_allowed_guesses = Game::calculate_number_allowed_guesses(&word);
 
         Game {
             word: word,
-            guesses: Vec::new(),
+            word_character_set: word_character_set,
             number_allowed_guesses: number_allowed_guesses,
+            guesses: Vec::new(),
+            guesses_character_set: HashSet::new(),
         }
     }
 
@@ -28,8 +34,9 @@ impl Game {
         min(word.len() + 10, 26)
     }
 
-    pub fn update_guess(mut self, guess: u8) {
+    pub fn update_guess(mut self, guess: char) {
         self.guesses.push(guess);
+        self.guesses_character_set.insert(guess);
     }
 }
 
